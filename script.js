83fa3ef97321d2a025f70d1ad8c45186e89f9dd7
ToboCodes <ccontreras.cumin@gmail.com>
2022-09-05 20:08:10 -0400

    Testing leafletjs map and location service.

---
+++ script.js
@@ -0,0 +1,34 @@
+let map = L.map('map', {
+  maxZoom: 18,
+  minZoom: 16,
+  maxBounds: [
+    [-39.6712,-72.9405],
+    [-39.6523, -72.9625]
+  ],
+}).setView([-39.66123, -72.95155], 16);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 18,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+let imageUrl = 'mapa 09-22.png',
+    imageBounds = [[-39.6523, -72.9625], [-39.6712,-72.9405]];
+L.imageOverlay(imageUrl, imageBounds).addTo(map);
+
+map.locate({setView: true, maxZoom: 17});
+
+function onLocationFound(e) {
+  let radius = e.accuracy;
+
+  L.marker(e.latlng).addTo(map);
+  L.marker(e.latlng).update();
+  L.circle(e.latlng, radius).addTo(map);
+}
+
+map.on('locationfound', onLocationFound);
+
+function onLocationError(e) {
+  alert(e.message);
+}
+
+map.on('locationerror', onLocationError);
