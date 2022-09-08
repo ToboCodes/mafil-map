993be4ffe31738f8c03152fed22fa5169aef7c8b
ToboCodes <ccontreras.cumin@gmail.com>
2022-09-07 21:58:04 -0400

    Improved accuracy and tile position. Testing location updates.

--- script.js
+++ script.js
@@ -1,31 +1,49 @@
+// Set map area and bounds
+const coord1 = [-39.6711,-72.9415];
+const coord2 = [-39.6517,-72.9614];
+let view = [-39.66123, -72.95155];
+
+// Create map and base tiles
 let map = L.map('map', {
   maxZoom: 18,
   minZoom: 16,
   maxBounds: [
-    [-39.6712,-72.9405],
-    [-39.6523, -72.9625]
+    coord1,
+    coord2
   ],
-}).setView([-39.66123, -72.95155], 16);
+}).setView(view, 16);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 18,
+    maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
+// Add custom layer
 let imageUrl = 'mapa 09-22.png',
-    imageBounds = [[-39.6523, -72.9625], [-39.6712,-72.9405]];
+    imageBounds = [coord2, coord1];
 L.imageOverlay(imageUrl, imageBounds).addTo(map);
 
-map.locate({setView: true, watch: true, maxZoom: 17});
+// Enable device GPS
+map.locate({watch: true, enableHighAccuracy: true, maximumAge: 2000});
+
+// Add location marker
+let switcher = 0;
 
 function onLocationFound(e) {
-  let radius = e.accuracy / 2;
-
-  L.marker(e.latlng).addTo(map);
-  L.circle(e.latlng, radius).addTo(map);
+  if (switcher == 0) {
+    L.marker(e.latlng).addTo(map);
+    let radius = e.accuracy / 2;
+    L.circle(e.latlng, radius).addTo(map);
+    switcher++;
+  } else {
+    L.marker.remove();
+    L.circle.remove();
+    switcher = 0;
+  }
 }
 
 map.on('locationfound', onLocationFound);
 
+// On location error
 function onLocationError(e) {
   alert(e.message);
 }
